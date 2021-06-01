2960513fd04a32993a44638f507145a34fd2fc0b
arunbhalli <tomarbhalli@gmail.com>
2021-06-02 01:14:19 +0200

    add new files Home nad Stared

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 const Navbar = () => {
+  const MyLinks = [
+    { to: '/', text: 'Home' },
+    { to: '/stared', text: 'Stared' },
+  ];
   return (
     <div>
       <ul>
-     <Link to='/stared'>Go to stared page</Link>
-      <li>
-      </li>
+        {MyLinks.map((item) => (
+          <li ket={item.to}>
+            <Link to={item.to}>{item.text}</Link>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
