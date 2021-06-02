e863b051e62294dc3e410b7256f9135e63f1beba
arunbhalli <tomarbhalli@gmail.com>
2021-06-02 02:40:09 +0200

    created input and fetch result from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Starred from './components/Starred';
 function App() {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import MainPageLayout from './MainPageLayout'
-const Home = () => {
-  return (
-    <MainPageLayout >
-      This is Home page
-    </MainPageLayout>
-  )
-}
+import React, { useState } from 'react';
+import MainPageLayout from './MainPageLayout';
 
-export default Home
+const Home = () => {
+  const [myInput, setMyInput] = useState('');
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${myInput}`)
+      .then((r) => r.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+  const onInputChange = (event) => {
+    setMyInput(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <MainPageLayout>
+      <input
+        value={myInput}
+        onchange={onInputChange}
+        onKeyDown={onKeyDown}
+        type='text'
+        autocomplete='off'
+      />
+      <button type='button' onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+};
+
+export default Home;
