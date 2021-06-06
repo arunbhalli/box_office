da249d9bc80acf63621adf1db3b6ce8feee7bbfe
arunbhalli <tomarbhalli@gmail.com>
2021-06-07 00:01:32 +0200

    created preview card for actor data from api

--- src/components/actor/ActorGrid.jsx
+++ src/components/actor/ActorGrid.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import ActorCard from './ActorCard';
+import IMAGE_NOT_FOUND from '../images/not-found.png';
 const ActorGrid = (data) => {
   return (
     <div>
       {data.map(({ person }) => (
-        <personCard
+        <ActorCard
           key={person.id}
-          id={person.id}
           name={person.name}
-          country={person.country ? person.country : null}
+          country={person.country ? person.country.name : null}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          gender={person.gender}
           image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
           summary={person.summary}
         />
